7c9fe2368292c0c71fabd645e0dba965dc767830
jesus mendez <jesusmendez@C02P11BAFY14-2.local>
2017-12-29 17:00:23 -0400

    +Spoilers and Details.

--- src/Components/SpoilerForm/SpoilerForm.jsx
+++ src/Components/SpoilerForm/SpoilerForm.jsx
@@ -6,8 +6,20 @@ class SpoilerForm extends Component{
     constructor(props){
         super(props);
         this.state = {
-            spoilers: props.content || []
+            spoilers: props.content || [],
+            newComment:this.props.reply
         }
+        this.addComment = this.addComment.bind(this);
+        this.handleUserInput = this.handleUserInput.bind(this);
+    }
+    addComment(){
+        this.props.reply(this.state.text);
+    }
+
+    handleUserInput(e){
+        this.setState({
+            text: e.target.value, // the value of the text input
+        })
     }
     componentWillReceiveProps(nextProps){
         this.setState({
@@ -20,8 +32,20 @@ class SpoilerForm extends Component{
             <div>
                  <hr />
                   {this.state.spoilers.map(spoiler=> 
-                        <Spoiler content={spoiler} key={spoiler.id} id={spoiler.id} />
+                        <div>
+                            <Spoiler content={spoiler} key={spoiler.id} id={spoiler.id} />
+                            <a onClick={this.addComment}>
+                                Reply
+                            </a>
+                        </div>
                     )}
+                    <div>
+                        <img src="" alt="" />
+                        <input className="comment" value={this.state.text} onChange={this.handleUserInput} />
+                        <button onClick={this.addComment} className="btn btn-primary">
+                            Comment
+                        </button>
+                    </div>
             </div>
         );
     }
--- src/Containers/DetailPage.jsx
+++ src/Containers/DetailPage.jsx
@@ -4,7 +4,7 @@ import Movie from '../Components/Movie/MovieInf';
 import SpoilerForm from "../Components/SpoilerForm/SpoilerForm";
 import { ELASTIC_API, basicApi }  from '../Config/Config'
 import elasticsearch from "elasticsearch";
-import { db } from "./../Config/Config";
+import { db, auth } from "./../Config/Config";
 
 
 class DetailPage extends Component {
@@ -18,6 +18,24 @@ class DetailPage extends Component {
             comments: []
         }
       };
+      this.addComment = this.addComment.bind(this);
+    }
+
+    addComment(comment){
+        console.log("added comment in spoiler");
+        db.collection("movies").add({
+            UserDisplayName: auth.currentUser.displayName,
+            UserId: auth.currentUser.id,
+            movieId: "4yhEmWAB72ZT2si3EXRn",
+            name: "",
+            text: ""
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
     }
 
     componentDidMount() {    
@@ -79,7 +97,7 @@ class DetailPage extends Component {
         return(
             <div className="container">
               <Movie content={this.state.Movie} />
-              <SpoilerForm content={this.state.comments} />
+              <SpoilerForm content={this.state.comments} reply ={this.addComment} key="spoilerForm" />
             </div>
         );
     }
