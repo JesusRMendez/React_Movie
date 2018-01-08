a86a72f1808b478a9666a4b7df97dc72d60b240f
jesus mendez <jesusmendez@C02P11BAFY14-2.local>
2018-01-08 11:16:15 -0400

    +Add elasticsearch to package.json + add travis file

--- src/Containers/HomePage.jsx
+++ src/Containers/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; 
 import MovieSearch from '../Components/MovieSearch/MovieSearch';
-
+import LoginForm from '../Components/LoginForm/LoginForm';
 
 class HomePage extends Component {
     constructor(props){
@@ -14,7 +14,12 @@ class HomePage extends Component {
         return(
             <div className="container">
                 <div className="col-md-12">
+                {this.state.isAuthenticated ? 
+                
                     <MovieSearch elasticUrl="http://localhost:9200/movies/movie/" />
+                    :
+                    <LoginForm />}
+                     <MovieSearch elasticUrl="http://localhost:9200/movies/movie/" />
                 </div>
             </div>
         );
